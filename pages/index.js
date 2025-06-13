e6b3e291286a21004f2b4164281922310b654df5
CaptivAI <usecaptivai@gmail.com>
2025-06-12 21:44:07 -0400

    Delete pages directory

--- pages/index.js
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', padding: '2rem' }}>
-      <h1>CaptivAI</h1>
-      <p>Smart Systems for Solo Creators and Digital Marketers</p>
-    </div>
-  );
-}
