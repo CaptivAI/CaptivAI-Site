e657f8127eb34ceba9beabe148a96da69420772c
CaptivAI <usecaptivai@gmail.com>
2025-06-12 21:38:54 -0400

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div style={{ fontFamily: 'sans-serif', textAlign: 'center', padding: '2rem' }}>
+      <h1>CaptivAI</h1>
+      <p>Smart Systems for Solo Creators and Digital Marketers</p>
+    </div>
+  );
+}
